2088a100ddb8b136bc4f1a657f7fbfe182404c70
Michael Bruty <michael.bruty@students.plymouth.ac.uk>
2020-10-29 17:18:58 +0000

    Resetting back to default

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -9,7 +9,6 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
-          Automatically updated from travis *for reals this time*
         </p>
         <a
           className="App-link"
