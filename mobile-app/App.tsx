fde1614ce7712d072064138d63221625e230633e
mbruty <57625243+mbruty@users.noreply.github.com>
2020-11-01 23:13:52 +0000

    Set-up rendering different pages

--- mobile-app/App.tsx
+++ mobile-app/App.tsx
@@ -4,12 +4,24 @@ import { IUser } from "./App/IUser";
 import { Preferences } from "./App/Preferences";
 
 export default function App() {
-  const [user, setUser] = useState<IUser>({fName: "Mike"});
-  return (
-    <SafeAreaView style={styles.container}>
-      <Preferences fName={user.fName}/>
-    </SafeAreaView>
-  );
+  const [user, setUser] = useState<IUser>({ fName: "Mike" });
+  const [page, setPage] = useState<string>("preferences");
+
+  switch (page) {
+    case "preferences":
+      return (
+        <SafeAreaView style={styles.container}>
+          <Preferences setPage={setPage} fName={user.fName} />
+        </SafeAreaView>
+      );
+      break;
+    default:
+      return (
+        <View style={styles.container}>
+          <Text>There was an error loading the app...</Text>
+        </View>
+      );
+  }
 }
 
 const styles = StyleSheet.create({
