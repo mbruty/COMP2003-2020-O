bfbbf13614716fe37f81a77c5771ff79d4c7f609
Oscar Davies <oscar.william.davies@outlook.com>
2020-11-05 10:38:06 +0000

    Base SQL Instance

---
+++ api/Backend/Data/SQL/SQLObj.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace api.Backend.Data.SQL
+{
+    public class SQLObj
+    {
+    }
+}
--- api/Program.cs
+++ api/Program.cs
@@ -9,14 +9,18 @@ namespace api
 
         private static void Main(string[] args)
         {
+            api.Backend.Data.SQL.SQLInstance.Start("root","tat","Jaminima48");
+
+            var p = api.Backend.Data.SQL.AutoSQL.tableColumns;
+
             // To run on dev server
-            if(args.Contains("-d"))
-                api.Backend.Endpoints.WebListener.Start(445);
-            // To run on the production server
-            else if(args.Contains("-p"))
-                api.Backend.Endpoints.WebListener.Start(444);
-            else
-                api.Backend.Endpoints.WebListener.Start();
+            //if(args.Contains("-d"))
+            //    api.Backend.Endpoints.WebListener.Start(445);
+            //// To run on the production server
+            //else if(args.Contains("-p"))
+            //    api.Backend.Endpoints.WebListener.Start(444);
+            //else
+            //    api.Backend.Endpoints.WebListener.Start();
 
             //Prevent The App Closing
             while (true) { Console.ReadLine(); }
