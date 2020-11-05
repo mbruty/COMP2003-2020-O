2ccfa1086868cc06549b383942ef7232568fc597
Oscar Davies <oscar.william.davies@outlook.com>
2020-11-05 15:22:41 +0000

    Example of sexy Getters for easy data access

--- api/Backend/Data/Obj/User.cs
+++ api/Backend/Data/Obj/User.cs
@@ -1,4 +1,6 @@
-﻿namespace api.Backend.Data.Obj
+﻿using api.Backend.Data.SQL.AutoSQL;
+
+namespace api.Backend.Data.Obj
 {
     public class User : Backend.Data.SQL.Object
     {
@@ -6,6 +8,11 @@
 
         public string Email, Password, Nickname;
         public int Id, YearOfBirth, CheckId;
+
+        public Visit[] visits
+        {
+            get { return Binding.GetTable<Visit>().Select<Visit>("UserId", Id); }
+        }
 
         #endregion Fields
     }
--- api/SQL Test Code.cs
+++ api/SQL Test Code.cs
@@ -10,11 +10,20 @@ namespace api
         public static void Run()
         {
             Binding.Add<User>("User");
-            Binding.Add<Resturant>("Resturant");
-            Binding.Add<Visit>("Visit");
-            Binding.Add<FoodChecks>("foodchecks");
+            //Binding.Add<Session>("Session");
+            Binding.Add<FoodChecks>("FoodChecks");
 
-            var p = Binding.GetTable<FoodChecks>().Select<FoodChecks>();
+            Binding.Add<Resturant>("Resturant");
+            //Binding.Add<FoodTags>("FoodTags");
+            //Binding.Add<MenuItem>("MenuItem");
+            //Binding.Add<MenuItemTags>("MenuItemTags");
+
+            //Binding.Add<FoodOpinion>("FoodOpinion");
+            //Binding.Add<ResturantOpinion>("ResturantOpinion");
+            Binding.Add<Review>("Review");
+            Binding.Add<Visit>("Visit");
+
+            var p = Binding.GetTable<User>().Select<User>()[0].visits;
         }
 
         #endregion Methods
