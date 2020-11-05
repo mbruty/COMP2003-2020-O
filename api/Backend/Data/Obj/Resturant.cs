4494ede03793ddf6e683aa8cda20c4943f9a8cdd
Oscar Davies <oscar.william.davies@outlook.com>
2020-11-05 13:57:41 +0000

    Update Gitignore to allow for /Data/obj folder

---
+++ api/Backend/Data/Obj/Resturant.cs
@@ -0,0 +1,12 @@
+﻿using api.Backend.Data.SQL;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace api.Backend.Data.Obj
+{
+    public class Resturant : Object
+    {
+    }
+}
---
+++ api/Backend/Data/Obj/User.cs
@@ -0,0 +1,12 @@
+﻿namespace api.Backend.Data.Obj
+{
+    public class User : Backend.Data.SQL.Object
+    {
+        #region Fields
+
+        public string Email, Password, Nickname;
+        public int Id, YearOfBirth, CheckId;
+
+        #endregion Fields
+    }
+}
