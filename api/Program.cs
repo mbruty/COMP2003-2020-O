8e6b90dc0cc689997a8f02c491b853c4d07f808c
Oscar Davies <oscar.william.davies@outlook.com>
2020-11-05 13:46:23 +0000

    Cleanup Main.cs

--- api/Program.cs
+++ api/Program.cs
@@ -1,5 +1,4 @@
-﻿using api.Backend.Data.SQL.AutoSQL;
-using System;
+﻿using System;
 using System.Linq;
 
 namespace api
@@ -10,59 +9,31 @@ namespace api
 
         private static void Main(string[] args)
         {
-            api.Backend.Data.SQL.Instance.Start("root", "tat", "Jaminima48");
+            //To run on dev server
+            if (args.Contains("-d"))
+            {
+                api.Backend.Data.SQL.Instance.Start("root", "tat", Backend.Security.SQL_Connection_Details.DevPword, "devsql.trackandtaste.com", "3307");
+                api.Backend.Endpoints.WebListener.Start(445);
+            }
+            // To run on the production server
+            else if (args.Contains("-p"))
+            {
+                api.Backend.Data.SQL.Instance.Start("root", "tat", Backend.Security.SQL_Connection_Details.DevPword, "prodsql.trackandtaste.com");
+                api.Backend.Endpoints.WebListener.Start(444);
+            }
+            //Run locally
+            else
+            {
+                api.Backend.Data.SQL.Instance.Start("root", "tat", "Jaminima48");
+                SQL_Test_Code.Run();
 
-            Table[] t = api.Backend.Data.SQL.AutoSQL.Instance.tables;
-
-            Binding.Add<User>("User");
-
-            var p = Binding.GetTable<User>().Select<User>();
-
-            p[0].Nickname = "Garath";
-            p[0].Update();
-
-            User u = new User();
-            u.Email = "oscar.d@gmai.com";
-            u.Password = "sdsfsdf";
-            u.YearOfBirth = 2002;
-            u.CheckId = 1;
-
-            u.Insert();
-
-            u = Binding.GetTable<User>().Select<User>("Email", u.Email)?.First();
-
-            u.Delete();
-
-            //User[] users = /*t[4].Select<User>(new object[] { null, "o.d@g.c" });*/
-            //t[4].Select<User>("YearOfBirth", 2001);
-
-            // To run on dev server
-            //if(args.Contains("-d"))
-            //    api.Backend.Endpoints.WebListener.Start(445);
-            //// To run on the production server
-            //else if(args.Contains("-p"))
-            //    api.Backend.Endpoints.WebListener.Start(444);
-            //else
-            //    api.Backend.Endpoints.WebListener.Start();
+                api.Backend.Endpoints.WebListener.Start();
+            }
 
             //Prevent The App Closing
             while (true) { Console.ReadLine(); }
         }
 
         #endregion Methods
-
-        #region Classes
-
-        private class User : Backend.Data.SQL.Object
-        {
-            #region Fields
-
-            public string Email, Password, Nickname;
-            public int Id, YearOfBirth, CheckId;
-
-            #endregion Fields
-        }
-
-        #endregion Classes
     }
 }
---
+++ api/SQL Test Code.cs
@@ -0,0 +1,40 @@
+﻿using api.Backend.Data.Obj;
+using api.Backend.Data.SQL.AutoSQL;
+using System.Linq;
+
+namespace api
+{
+    public static class SQL_Test_Code
+    {
+        #region Methods
+
+        public static void Run()
+        {
+            Table[] t = api.Backend.Data.SQL.AutoSQL.Instance.tables;
+
+            Binding.Add<User>("User");
+
+            var p = Binding.GetTable<User>().Select<User>();
+
+            p[0].Nickname = "Garath";
+            p[0].Update();
+
+            User u = new User();
+            u.Email = "oscar.d@gmai.com";
+            u.Password = "sdsfsdf";
+            u.YearOfBirth = 2002;
+            u.CheckId = 1;
+
+            u.Insert();
+
+            u = Binding.GetTable<User>().Select<User>("Email", u.Email)?.First();
+
+            u.Delete();
+
+            User[] users = /*t[4].Select<User>(new object[] { null, "o.d@g.c" });*/
+            t[4].Select<User>("YearOfBirth", 2001);
+        }
+
+        #endregion Methods
+    }
+}
