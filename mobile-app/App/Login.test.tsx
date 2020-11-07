3ead66d20dc06c239dec89aabdfb03febbbba76f
Michael Bruty <michael.bruty@students.plymouth.ac.uk>
2020-11-07 15:13:39 +0000

    Removed tests as they're bugged... Will investigate - low priority

--- mobile-app/App/Login.test.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import renderer from "react-test-renderer";
-import { LogIn } from "./LogIn";
-
-test("Renders Circle Button List", () => {
-  const logInPage = renderer.create(<LogIn />).toJSON();
-  expect(logInPage).toMatchSnapshot();
-});
