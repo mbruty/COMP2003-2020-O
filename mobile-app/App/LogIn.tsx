964ba8a9a0997435e5ae3e5b89abb9641870c02c
AlexDenman47674 <alexander.denman@students.plymouth.ac.uk>
2020-11-07 14:18:20 +0000

    Login Screen Mk2.1
    Created non functional login screen with all necessary elements including image, log in button, on touch opacity text as well as Email and password fields.

--- mobile-app/App/LogIn.tsx
+++ mobile-app/App/LogIn.tsx
@@ -1,15 +1,67 @@
 import React from "react";
-import { ScrollView, StyleSheet, Text } from "react-native";
+import {Alert, Button, Dimensions, Image, ScrollView, StyleSheet, Text, TouchableOpacity, View} from "react-native";
 import { CustomTextInput } from "./Login/CustomTextInput";
+import {CONSTANT_STYLES} from "./constants";
 
 interface Props {}
 
 export const LogIn: React.FC<Props> = (props) => {
+  // @ts-ignore
+  const dimensions= Dimensions.get("window");
+  const imageHeight=Math.round((dimensions.width*8)/13);
+  const imageWidth=dimensions.width;
+
+  const styles = StyleSheet.create({
+    ImageStyle:{
+      height:imageHeight,
+      width:imageWidth,
+    }
+
+  })
+
+
   return (
     <>
-      <Text>Hello!!!!!</Text>
-      <CustomTextInput label="Username or Email" isPassword={false} />
-      <CustomTextInput label="Password" isPassword={true} />
+      <View style={{
+        top:-175,
+        backgroundColor:'white'
+      }}>
+        <Image style={styles.ImageStyle} source={require('./log-in.png')}/>
+      </View>
+
+
+      <View style={{
+        position:"absolute",
+        top: 200,
+        borderRadius:40,
+        backgroundColor:'white',
+        borderWidth:50,
+        borderColor:'white'
+      }}>
+
+        <Text style={{fontWeight:"bold" , color:'red' , fontSize:24 , paddingBottom:15 , paddingTop:10 , textAlign: 'center'}}>Track and Taste</Text>
+
+
+        <CustomTextInput label="Email" isPassword={false} />
+        <CustomTextInput label="Password" isPassword={true} />
+
+
+        <TouchableOpacity>
+          <Text style={{color:'firebrick' , fontSize:12 , textAlign: "right" , paddingBottom:15}}>Forgot Password?</Text>
+        </TouchableOpacity>
+
+        <Button title={"Log In"} onPress={console.clear} color={'red'}>Log In</Button>
+
+
+        <Text style={{color:"gainsboro" , paddingTop:15}}>
+          Don't have an account?
+          <TouchableOpacity>
+            <Text style={{color:"firebrick" , textAlign: "right"}}> Sign Up</Text>
+          </TouchableOpacity>
+        </Text>
+      </View>
+
+
     </>
   );
 };
