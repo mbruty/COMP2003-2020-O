e982df470e5d3834efc9c7f878f780b86f2d3392
Michael Bruty <michael.bruty@students.plymouth.ac.uk>
2020-11-08 15:29:02 +0000

    File restructure

---
+++ mobile-app/App/shared/constants.ts
@@ -0,0 +1,34 @@
+import { StyleSheet } from "react-native";
+
+export const CONSTANT_STYLES = StyleSheet.create({
+  BG_BASE_COLOUR: {
+    backgroundColor: "#F1F1F1",
+  },
+  BG_RED: {
+    backgroundColor: "#FD4040",
+  },
+  BG_GREEN: {
+    backgroundColor: "#65B016",
+  },
+  BG_WHITE: {
+    backgroundColor: "#FFF",
+  },
+  BG_LIGHT_GREY: {
+    backgroundColor: "#B6B6B6",
+  },
+  BG_DARK_GREY: {
+    backgroundColor: "#707070",
+  },
+  TXT_DEFAULT: {
+    color: "#707070",
+  },
+  TXT_WHITE: {
+    color: '#FFF'
+  },
+  TXT_BASE: {
+    color: '#F1F1F1'
+  },
+  TXT_RED: {
+    color: '#FD4040'
+  }
+});
