23f1b258939eb0ba8d376f0362518335dbc1afd0
AlexDenman47674 <alexander.denman@students.plymouth.ac.uk>
2020-11-10 17:10:49 +0000

    Added Sign Up Page to app
    Email, username, password and password check working, date picker is WIP.

--- mobile-app/App/SignUpProcess/SignUp.tsx
+++ mobile-app/App/SignUpProcess/SignUp.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
+import DatePicker from 'react-native-date-picker';
 import {
+  DatePickerAndroid, DatePickerIOS, DatePickerIOSComponent,
   Dimensions,
   Image,
   ScrollView,
@@ -12,18 +14,22 @@ import { CONSTANT_STYLES } from "./shared/constants";
 import { PasswordInput } from "./SignUp/PasswordInput";
 import { FormProgress } from "./shared/FormProgress";
 
+
 interface Props {}
 interface SignUp {
   nick: string;
   email: string;
   password: string;
 }
+
 export const SignUp: React.FC<Props> = (props) => {
   const dimensions = Dimensions.get("window");
   const imageHeight = Math.round((dimensions.width * 9) / 16);
   const imageWidth = dimensions.width;
 
   const [values, setValues] = useState<SignUp | undefined>();
+  const [date, setDate] = useState(new Date());
+
 
   return (
     <ScrollView>
@@ -39,17 +45,28 @@ export const SignUp: React.FC<Props> = (props) => {
         allowFontScaling={false}
         style={[styles.bannerText, CONSTANT_STYLES.TXT_BASE]}
       >
-        {"Hello,\nLet's get your account\nset-up"}
+        {"Hello,\nLet's get your account \nset-up"}
       </Text>
       <View style={[styles.txtContainer, { width: imageWidth - 100 }]}>
         <AwesomeTextInput
           label="Email"
           customStyles={{
             title: CONSTANT_STYLES.TXT_DEFAULT,
-            container: { marginTop: 25 },
+            container: { marginTop: 5 },
           }}
           onChangeText={(text) => setValues({ ...values, email: text })}
         />
+        <AwesomeTextInput
+            label="Username"
+            customStyles={{
+              title: CONSTANT_STYLES.TXT_DEFAULT,
+              container:{marginTop:25}
+            }}
+            onChangeText={(text) => setValues({...values, nick: text})}
+        />
+
+        <DatePicker date={date} onDateChange={setDate} />
+
         <PasswordInput
           label="Password"
           customStyles={{
@@ -62,7 +79,7 @@ export const SignUp: React.FC<Props> = (props) => {
           label="Repeat Password"
           customStyles={{
             title: CONSTANT_STYLES.TXT_DEFAULT,
-            container: { marginTop: 25 },
+            container: { marginTop: 25 , marginBottom:25},
           }}
           onChangeText={() => {
             //ToDo: Validate password
@@ -79,7 +96,7 @@ const styles = StyleSheet.create({
     position: "absolute",
     top: 35,
     left: 15,
-    fontSize: 30,
+    fontSize: 25,
     fontWeight: "bold",
     elevation: 26,
   },
