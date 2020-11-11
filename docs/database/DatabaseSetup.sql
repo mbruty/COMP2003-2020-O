31fcd7d73d19cbc8f98041fe43b5722264b83688
Oscar Davies <oscar.william.davies@outlook.com>
2020-11-11 16:57:51 +0000

    Fix Resturant -> Restaurant

--- docs/database/DatabaseSetup.sql
+++ docs/database/DatabaseSetup.sql
@@ -1,11 +1,11 @@
 Drop table if exists `Review`;
 Drop table if exists `Visit`;
 Drop table if exists `FoodOpinion`;
-Drop table if exists `ResturantOpinion`;
+Drop table if exists `RestaurantOpinion`;
 Drop table if exists `MenuItemTags`;
 Drop table if exists `MenuItem`;
 Drop table if exists `FoodTags`;
-Drop table if exists `Resturant`;
+Drop table if exists `Restaurant`;
 Drop table if exists `Session`;
 Drop table if exists `User`;
 Drop table if exists `FoodChecks`;
@@ -51,7 +51,7 @@ create table `Session` (
     AuthToken varchar(110) not null
 );
 
-create table `Resturant` (
+create table `Restaurant` (
 	Id int unique auto_increment not null,
     primary key (Id),
     
@@ -80,7 +80,7 @@ create table `MenuItem` (
 	ResturantId int not null,
 	CheckId int not null,
     foreign key (CheckId) references `FoodChecks`(Id) on delete restrict on update cascade,
-    foreign key (ResturantId) references `Resturant`(Id) on delete restrict on update cascade,
+    foreign key (ResturantId) references `Restaurant`(Id) on delete restrict on update cascade,
     
     `Name` varchar(30) not null,
     `Description` varchar(120) not null,
@@ -114,12 +114,12 @@ create table `FoodOpinion` (
     NeverShow bit default 0
 );
 
-create table `ResturantOpinion` (
+create table `RestaurantOpinion` (
 	UserId int not null,
 	ResturantId int not null,
     primary key (UserId, ResturantId),
     foreign key (UserId) references `User`(Id) on delete restrict on update cascade,
-    foreign key (ResturantId) references `Resturant`(Id) on delete restrict on update cascade,
+    foreign key (ResturantId) references `Restaurant`(Id) on delete restrict on update cascade,
     
     SwipeLeft int default 0,
     SwipeRight int default 0,
@@ -133,7 +133,7 @@ create table `Visit` (
 	UserId int not null,
 	ResturantId int not null,
     foreign key (UserId) references `User`(Id) on delete restrict on update cascade,
-    foreign key (ResturantId) references `Resturant`(Id) on delete restrict on update cascade,
+    foreign key (ResturantId) references `Restaurant`(Id) on delete restrict on update cascade,
     
     GroupSize TinyInt not null,
     check (GroupSize>0),
