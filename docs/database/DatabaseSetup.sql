4ce6f7008d1b231a18622e54d37e16753de53475
ReefLakin <reeflakin2@gmail.com>
2020-11-19 21:40:35 +0000

    Database Rebuild (Part 3)
    The menu system has gotten a complete overhaul, allowing for far more customisability within restaurant menus. This aids restaurant chains greatly and ensures we don't reccomend the customer food they can't have, either due to the date or time.
    
    (See Logbook notes from 19th Nov. for more information.)

--- docs/database/DatabaseSetup.sql
+++ docs/database/DatabaseSetup.sql
@@ -1,9 +1,14 @@
 Drop table if exists `Review`;
 Drop table if exists `Visit`;
 Drop table if exists `RestaurantOpinion`;
-Drop table if exists `MenuItemTags`;
-Drop table if exists `MenuItem`;
 
+DROP TABLE IF EXISTS `LinkMenuFood`;
+DROP TABLE IF EXISTS `FoodItemTags`;
+DROP TABLE IF EXISTS `FoodItem`;
+DROP TABLE IF EXISTS `MenuTimes`;
+DROP TABLE IF EXISTS `LinkMenuRestaurant`;
+DROP TABLE IF EXISTS `Menu`;
+DROP TABLE IF EXISTS `OpeningHours`;
 DROP TABLE IF EXISTS `Days`;
 DROP TABLE IF EXISTS `FoodOpinion`;
 DROP TABLE IF EXISTS `FoodTags`;
@@ -118,32 +123,102 @@ CREATE TABLE `Days` (
     CONSTRAINT UNQ_Days UNIQUE (DayName)
 );
 
+CREATE TABLE `OpeningHours` (
+    RestaurantID INT NOT NULL,
+    DayRef VARCHAR(5) NOT NULL,
+    OpenTime TIME NOT NULL DEFAULT '08:00:00',
+    CloseTime TIME NOT NULL DEFAULT '21:00:00',
 
+    PRIMARY KEY (RestaurantID, DayRef),
+    CONSTRAINT UNQ_OpeningHours UNIQUE (RestaurantID, DayRef),
 
+    CONSTRAINT FK_RestaurantInOpeningHours FOREIGN KEY (RestaurantID)
+        REFERENCES Restaurant(RestaurantID) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT FK_DayInOpeningHours FOREIGN KEY (DayRef)
+        REFERENCES `Days`(DayRef) ON UPDATE CASCADE ON DELETE RESTRICT,
+    CONSTRAINT CHK_OpenTime CHECK (OpenTime BETWEEN '00:00:00' AND '24:00:00'),
+    CONSTRAINT CHK_CloseTime CHECK (CloseTime BETWEEN '00:00:00' AND '24:00:00'),
+    CONSTRAINT CHK_OpeningTimeRange CHECK (OpenTime < CloseTime)
+);
 
+CREATE TABLE `Menu` (
+    MenuID INT NOT NULL AUTO_INCREMENT,
+    MenuName VARCHAR(20) NOT NULL DEFAULT 'Menu',
+    IsChildMenu BIT NOT NULL DEFAULT 0,
 
-create table `MenuItem` (
-	Id int unique auto_increment not null,
-	primary key (Id),
-    
-	ResturantId int not null,
-	CheckId int not null,
-    foreign key (CheckId) references `FoodChecks`(Id) on delete restrict on update cascade,
-    foreign key (ResturantId) references `Restaurant`(Id) on delete restrict on update cascade,
-    
-    `Name` varchar(30) not null,
-    `Description` varchar(120) not null,
-    Price float not null
+    PRIMARY KEY (MenuID),
+    CONSTRAINT UNQ_Menu UNIQUE (MenuID)
+);
+
+CREATE TABLE `LinkMenuRestaurant` (
+    MenuRestID INT NOT NULL AUTO_INCREMENT,
+    MenuID INT NOT NULL,
+    RestaurantID INT NOT NULL,
+    AlwaysServe BIT NOT NULL DEFAULT 1,
+
+    PRIMARY KEY (MenuRestID),
+    CONSTRAINT UNQ_LinkMenuRestaurant UNIQUE (MenuID, RestaurantID),
+
+    CONSTRAINT FK_MenuInMenuRestaurantLink FOREIGN KEY (MenuID)
+        REFERENCES Menu(MenuID) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT FK_RestaurantInMenuRestaurantLink FOREIGN KEY (RestaurantID)
+        REFERENCES Restaurant(RestaurantID) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+CREATE TABLE `MenuTimes` (
+    MenuRestID INT NOT NULL,
+    DayRef VARCHAR(5) NOT NULL,
+    StartServing TIME NOT NULL,
+    StopServing TIME NOT NULL,
+
+    PRIMARY KEY (MenuRestID, DayRef),
+
+    CONSTRAINT FK_MenuRestLinkInTimes FOREIGN KEY (MenuRestID)
+        REFERENCES LinkMenuRestaurant(MenuRestID) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT FK_DaysInMenuTimes FOREIGN KEY (DayRef)
+        REFERENCES `Days`(DayRef) ON UPDATE CASCADE ON DELETE RESTRICT
+);
+
+CREATE TABLE `FoodItem` (
+    FoodID INT NOT NULL AUTO_INCREMENT,
+    FoodCheckID INT NOT NULL,
+    FoodName VARCHAR(45) NOT NULL,
+    FoodDescription VARCHAR(120) NOT NULL,
+    Price DECIMAL(8,2) NOT NULL DEFAULT 0.00,
+
+    PRIMARY KEY (FoodID),
+
+    CONSTRAINT FK_FoodCheckInFoodItem FOREIGN KEY (FoodCheckID)
+        REFERENCES FoodChecks(FoodCheckID) ON UPDATE CASCADE ON DELETE RESTRICT,
+    CONSTRAINT CHK_FoodPrice CHECK (Price > 0.00)
+);
+
+CREATE TABLE `FoodItemTags` (
+    FoodID INT NOT NULL,
+    TagID INT NOT NULL,
+
+    PRIMARY KEY (FoodID, TagID),
+
+    CONSTRAINT FK_FoodInFoodItemTags FOREIGN KEY (FoodID)
+        REFERENCES FoodItem(FoodID) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT FK_TagInFoodItemTags FOREIGN KEY (TagID)
+        REFERENCES FoodTags(FoodTagID) ON UPDATE CASCADE ON DELETE RESTRICT
+);
+
+CREATE TABLE `LinkMenuFood` (
+    MenuID INT NOT NULL,
+    FoodID INT NOT NULL,
+
+    PRIMARY KEY (MenuID, FoodID),
+
+    CONSTRAINT FK_MenuInMenuFoodLink FOREIGN KEY (MenuID)
+        REFERENCES Menu(MenuID) ON UPDATE CASCADE ON DELETE CASCADE,
+    CONSTRAINT FK_FoodInMenuFoodLink FOREIGN KEY (FoodID)
+        REFERENCES FoodItem(FoodID) ON UPDATE CASCADE ON DELETE CASCADE
 );
 
 
-create table `MenuItemTags` (
-	MenuId int not null,
-    FoodTagId int not null,
-    primary key (MenuId, FoodTagId),
-    foreign key (MenuId) references `MenuItem`(Id) on delete restrict on update cascade,
-    foreign key (FoodTagId) references `FoodTags`(Id) on delete restrict on update cascade
-);
+
 
 
 
