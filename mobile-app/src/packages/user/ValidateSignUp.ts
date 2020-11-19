6149dc0508c8d9fb1626e3079f28849f4b3a02e6
Michael Bruty <michael.bruty@students.plymouth.ac.uk>
2020-11-19 14:31:36 +0000

    Altered how validate sign up works to make it easier to test

--- mobile-app/src/packages/user/ValidateSignUp.ts
+++ mobile-app/src/packages/user/ValidateSignUp.ts
@@ -1,11 +1,14 @@
 import { API_URL } from "../../constants";
 import { email, isToday, Values } from "./utils";
 
+const pwRegex = RegExp(
+  /^(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[!"#\$%&'\(\)\*\+,-\.\/:;<=>\?@[\]\^_`\{\|}~])[a-zA-Z0-9!"#\$%&'\(\)\*\+,-\.\/:;<=>\?@[\]\^_`\{\|}~]{8,}$/
+);
+
 const validate = async (
   values: Values,
   date: Date,
-  setErrors: React.Dispatch<React.SetStateAction<Values>>,
-  next: (value: string, userid: string, authtoken: string) => void
+  next?: (value: string, userid: string, authtoken: string) => void
 ) => {
   let hasErrors = false;
   let currentErrors: Values = {
@@ -31,13 +34,26 @@ const validate = async (
     currentErrors.email = "Email is required";
     hasErrors = true;
   }
-  if (values.password !== values.confPassword || values.password.length === 0) {
-    currentErrors.confPassword = "The passwords do not match";
+  if (values.password.length === 0 || values.confPassword.length === 0) {
+    currentErrors.confPassword = "Passwords cannot be empty\n";
+    hasErrors = true;
+  } else if (values.password !== values.confPassword) {
+    currentErrors.confPassword = "The passwords do not match\n";
+    hasErrors = true;
+  }
+  if (!pwRegex.test(values.password)) {
+    currentErrors.confPassword +=
+      "Please enter a strong password\n8 Characters in length; Atleast:\n\t1 Number,\n\t1 Symbol,\n\t1 Uppercase Character,\n\t1 Lowercase Character";
     hasErrors = true;
   }
   // date is initalised to today, so if it's today the haven't changed it
   if (isToday(date)) {
     currentErrors.dob = "Date of Birth is required";
+    hasErrors = true;
+  }
+  // Date isn't in the future
+  if (date > new Date()) {
+    currentErrors.dob = "Date of Birth cannot be in the future";
     hasErrors = true;
   }
   // No errors occurred... Time to send it to the api
@@ -67,7 +83,7 @@ const validate = async (
       })
       .catch((e) => alert("There was an error creating your account"));
   }
-  setErrors(currentErrors);
+  return currentErrors;
 };
 
 export default validate;
