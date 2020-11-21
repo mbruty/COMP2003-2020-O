ed3dd9136522b2824e7e04accaa47fbc786c0fee
Michael Bruty <michael.bruty@students.plymouth.ac.uk>
2020-11-21 13:45:03 +0000

    Removed unneeded import

--- mobile-app/src/hooks/useToggle.ts
+++ mobile-app/src/hooks/useToggle.ts
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
 const useToggle = (defaultVal: boolean) => {
   const [state, setState] = useState<boolean>(defaultVal);
