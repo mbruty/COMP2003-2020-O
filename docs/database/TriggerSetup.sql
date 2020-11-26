5bacfc163a635eb67b68b973bf63e5c98062229b
ReefLakin <reeflakin2@gmail.com>
2020-11-26 11:09:09 +0000

    Creation of TriggerSetup.sql
    An almost blank SQL script file is created for constructing the new database triggers.

---
+++ docs/database/TriggerSetup.sql
@@ -0,0 +1,6 @@
+/*
+SQL Trigger Setup:
+    The following script should create the necessary triggers associated with our project.
+    It will delete triggers if they currently exist in the database.
+    The triggers are explained within this comment bracket of the script.
+*/
