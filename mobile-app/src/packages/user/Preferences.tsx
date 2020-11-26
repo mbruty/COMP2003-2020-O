f120f039ab4014bea7e2e2f1dd6074e10566ba32
Michael Bruty <michael.bruty@students.plymouth.ac.uk>
2020-11-26 13:51:00 +0000

    Added like / dislike logic

--- mobile-app/src/packages/user/Preferences.tsx
+++ mobile-app/src/packages/user/Preferences.tsx
@@ -11,6 +11,7 @@ import {
 } from "react-native";
 import { CONSTANT_COLOURS, CONSTANT_STYLES } from "../../constants";
 import useToggle from "../../hooks/useToggle";
+import { ChipView } from "../controls";
 import AwesomeAutoCompleteInput from "../controls/AwesomeAutoCompleteInput";
 
 interface Props {}
@@ -20,6 +21,9 @@ let timeout;
 
 const Preferences: React.FC<Props> = (props) => {
   const [value, setValue] = useState<string>("");
+
+  const [likes, setLikes] = useState<Array<string> | undefined>();
+  const [dislikes, setDislikes] = useState<Array<string>>();
 
   const [showModal, toggleModal] = useToggle(false);
 
@@ -34,54 +38,119 @@ const Preferences: React.FC<Props> = (props) => {
     setValue(text);
   };
 
+  const onLike = (tag: string): void => {
+    if (likes?.includes(tag)) return;
+    if (dislikes?.includes(tag)) removeDislikeString(tag);
+    if (likes) setLikes([...likes, tag]);
+    else setLikes([tag]);
+  };
+
+  const onDisLike = (tag: string): void => {
+    if (dislikes?.includes(tag)) return;
+    if (likes?.includes(tag)) removeLikeString(tag);
+    if (dislikes) setDislikes([...dislikes, tag]);
+    else setDislikes([tag]);
+  };
+
+  const removeLike = (index: number): void => {
+    const newLikes = likes.filter((_, idx) => idx !== index);
+
+    if (newLikes.length === 0) {
+      setLikes(undefined);
+    } else {
+      setLikes(newLikes);
+    }
+  };
+
+  const removeLikeString = (tag: string): void => {
+    // Filter everything that isn't the value
+    const newLikes = likes.filter((value) => value !== tag);
+
+    if (newLikes.length === 0) {
+      setLikes(undefined);
+    } else {
+      setLikes(newLikes);
+    }
+  };
+
+  const removeDislike = (index: number): void => {
+    const newDislikes = dislikes.filter((_, idx) => idx !== index);
+
+    if (newDislikes.length === 0) {
+      setDislikes(undefined);
+    } else {
+      setDislikes(newDislikes);
+    }
+  };
+
+  const removeDislikeString = (tag: string): void => {
+    // Filter everything that isn't the value
+    const newDislikes = dislikes.filter((value) => value !== tag);
+
+    if (newDislikes.length === 0) {
+      setDislikes(undefined);
+    } else {
+      setDislikes(newDislikes);
+    }
+  };
   return (
-    <ScrollView contentContainerStyle={styles.container}>
-      <Modal animationType="fade" transparent={true} visible={showModal}>
-        <TouchableWithoutFeedback onPress={toggleModal}>
-          <View style={styles.centeredView}>
-            <TouchableWithoutFeedback onPress={() => {}}>
-              <View style={styles.modalView}>
-                <TouchableOpacity
-                  onPress={toggleModal}
-                  style={{
-                    alignSelf: "flex-end",
-                    marginTop: -15,
-                  }}
-                >
-                  <AntDesign name="closecircleo" size={24} color="#AAA" />
-                </TouchableOpacity>
-                <Text style={{marginTop: -15}}>
-                  We won't show you any reccomendations that contains these tags
-                </Text>
-              </View>
-            </TouchableWithoutFeedback>
-          </View>
-        </TouchableWithoutFeedback>
-      </Modal>
-      <AwesomeAutoCompleteInput
-        autoComplete={values}
-        value={value}
-        setValue={debounceInput}
-      />
-      <Text style={[styles.txt, CONSTANT_STYLES.TXT_DEFAULT]}>
-        I like to eat
-      </Text>
-      <View style={styles.seperator} />
-      <View style={{ alignSelf: "flex-start" }}>
-        <TouchableOpacity
-          style={styles.txtContainer}
-          onPress={() => toggleModal()}
-        >
-          <Text style={[styles.txt, CONSTANT_STYLES.TXT_DEFAULT]}>
-            I don't like to eat
-          </Text>
-          <Ionicons
-            name="ios-information-circle-outline"
-            size={24}
-            color={CONSTANT_COLOURS.DARK_GREY}
-            style={{ paddingTop: 5, paddingLeft: 5 }}
-          />
-        </TouchableOpacity>
+    <ScrollView>
+      <View style={styles.container}>
+        <Modal animationType="fade" transparent={true} visible={showModal}>
+          <TouchableWithoutFeedback onPress={toggleModal}>
+            <View style={styles.centeredView}>
+              <TouchableWithoutFeedback onPress={() => {}}>
+                <View style={styles.modalView}>
+                  <TouchableOpacity
+                    onPress={toggleModal}
+                    style={{
+                      alignSelf: "flex-end",
+                      marginTop: -15,
+                    }}
+                  >
+                    <AntDesign name="closecircleo" size={24} color="#AAA" />
+                  </TouchableOpacity>
+                  <Text style={{ marginTop: -15 }}>
+                    We won't show you any reccomendations that contains these
+                    tags
+                  </Text>
+                </View>
+              </TouchableWithoutFeedback>
+            </View>
+          </TouchableWithoutFeedback>
+        </Modal>
+        <AwesomeAutoCompleteInput
+          autoComplete={values}
+          value={value}
+          setValue={debounceInput}
+          onLike={onLike}
+          onDisLike={onDisLike}
+        />
+        <Text style={[styles.txt, CONSTANT_STYLES.TXT_DEFAULT]}>
+          I like to eat
+        </Text>
+        {likes && <ChipView chipNameList={likes} setTouched={removeLike} />}
+
+        <View style={styles.seperator} />
+        <View style={{ alignSelf: "flex-start" }}>
+          <TouchableOpacity
+            style={styles.txtContainer}
+            onPress={() => toggleModal()}
+          >
+            <Text style={[styles.txt, CONSTANT_STYLES.TXT_DEFAULT]}>
+              I don't like to eat
+            </Text>
+            <Ionicons
+              name="ios-information-circle-outline"
+              size={24}
+              color={CONSTANT_COLOURS.DARK_GREY}
+              style={{ paddingTop: 5, paddingLeft: 5 }}
+            />
+          </TouchableOpacity>
+          {dislikes && (
+            <ChipView chipNameList={dislikes} setTouched={removeDislike} />
+          )}
+        </View>
       </View>
     </ScrollView>
   );
