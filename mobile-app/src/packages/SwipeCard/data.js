aef4f06b30fea01741dd6a5d8ce3ddba6d1cc829
Michael Bruty <michael.bruty@students.plymouth.ac.uk>
2020-11-27 16:00:07 +0000

    Fixed spelling error

--- mobile-app/src/packages/SwipeCard/data.js
+++ mobile-app/src/packages/SwipeCard/data.js
@@ -5,7 +5,7 @@ export default [
     image:
       "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.readersdigest.ca%2Fwp-content%2Fuploads%2F2015%2F11%2Fgourmet-burger.jpg&f=1&nofb=1",
     tags: [
-      { text: "Veggetarian", enabled: true },
+      { text: "Vegetarian", enabled: true },
       { text: "Gluten Free", enabled: true },
       { text: "5 Restaurants have this in your area!", enabled: true },
     ],
@@ -16,18 +16,18 @@ export default [
     image:
       "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FCxZM0PM77Us%2Fmaxresdefault.jpg&f=1&nofb=1",
     tags: [
-      { text: "Veggetarian", enabled: true },
+      { text: "Vegetarian", enabled: true },
       { text: "Gluten Free", enabled: true },
       { text: "5 Restaurants have this in your area!", enabled: true },
     ],
   },
-    {
+  {
     id: 3,
     name: "BURGER",
     image:
       "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.readersdigest.ca%2Fwp-content%2Fuploads%2F2015%2F11%2Fgourmet-burger.jpg&f=1&nofb=1",
     tags: [
-      { text: "Veggetarian", enabled: true },
+      { text: "Vegetarian", enabled: true },
       { text: "Gluten Free", enabled: true },
       { text: "5 Restaurants have this in your area!", enabled: true },
     ],
@@ -38,18 +38,18 @@ export default [
     image:
       "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FCxZM0PM77Us%2Fmaxresdefault.jpg&f=1&nofb=1",
     tags: [
-      { text: "Veggetarian", enabled: true },
+      { text: "Vegetarian", enabled: true },
       { text: "Gluten Free", enabled: true },
       { text: "5 Restaurants have this in your area!", enabled: true },
     ],
   },
-    {
+  {
     id: 5,
     name: "BURGER",
     image:
       "https://external-content.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.readersdigest.ca%2Fwp-content%2Fuploads%2F2015%2F11%2Fgourmet-burger.jpg&f=1&nofb=1",
     tags: [
-      { text: "Veggetarian", enabled: true },
+      { text: "Vegetarian", enabled: true },
       { text: "Gluten Free", enabled: true },
       { text: "5 Restaurants have this in your area!", enabled: true },
     ],
@@ -60,7 +60,7 @@ export default [
     image:
       "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.ytimg.com%2Fvi%2FCxZM0PM77Us%2Fmaxresdefault.jpg&f=1&nofb=1",
     tags: [
-      { text: "Veggetarian", enabled: true },
+      { text: "Vegetarian", enabled: true },
       { text: "Gluten Free", enabled: true },
       { text: "5 Restaurants have this in your area!", enabled: true },
     ],
