be7822b31c239a59a7678d36fc902e932d8af84d
Michael Bruty <michael.bruty@students.plymouth.ac.uk>
2020-12-02 10:52:56 +0000

    Created basic theme

--- website/src/App.tsx
+++ website/src/App.tsx
@@ -1,25 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useMemo } from "react";
+import Nav from "./Nav";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
+import "./App.css";
 
 function App() {
+  const prefersDarkMode = useMediaQuery("(prefers-color-scheme: dark)");
+  console.log(prefersDarkMode);
+
+  const theme = useMemo(
+    () =>
+      createMuiTheme({
+        palette: {
+          background: {
+            paper: prefersDarkMode ? "#424242" : "#FFF",
+            default: prefersDarkMode ? "#303030" : "#FFF",
+          },
+          primary: {
+            light: "#7986cb",
+            main: "#3f51b5",
+            dark: "#303f9f",
+          },
+          type: prefersDarkMode ? "dark" : "light",
+        },
+      }),
+    [prefersDarkMode]
+  );
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={theme}>
+      <div
+        className="App"
+        style={{ backgroundColor: theme.palette.background.default }}
+      >
+        <Nav />
+        <p>{prefersDarkMode.toString()}</p>
+        <h1>hello</h1>
+      </div>
+    </ThemeProvider>
   );
 }
 
