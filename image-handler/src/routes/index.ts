ddd0c96e5885f92932e7f2d34e92535472baedf4
Michael Bruty <michael.bruty@students.plymouth.ac.uk>
2020-12-02 00:05:26 +0000

    Renamed the route to just /v0/

--- image-handler/src/routes/index.ts
+++ image-handler/src/routes/index.ts
@@ -2,7 +2,7 @@ import v0 from "./V0/api.v0";
 import express from "express";
 
 const router = express.Router();
-router.use("/api/v0", v0)
+router.use("/v0", v0)
 router.get("/test", (req, res) => {
   res.send("/test");
 });
