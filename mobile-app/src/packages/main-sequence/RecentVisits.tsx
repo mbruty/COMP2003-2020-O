03d9008c07688412b0c6dcdacca99ecc6053f72f
lukemann04 <34482771+lukemann04@users.noreply.github.com>
2020-12-02 10:45:05 +0000

    Tidied up code
    Removed test restaurants and other unnecessary lines of code.

--- mobile-app/src/packages/main-sequence/RecentVisits.tsx
+++ mobile-app/src/packages/main-sequence/RecentVisits.tsx
@@ -12,69 +12,7 @@ interface Props {
   restaurantDetails: Array<Details>;
 }
 
-// Type is a number corresponding to the card style
-// See resources/card(n).png
-/*const cards = [
-  {
-    index: 1,
-    type: 0,
-    name: "Restaurant 1",
-    visitDate: "09-12-20",
-  },
-  {
-    index: 2,
-    type: 1,
-    name: "Restaurant 2",
-    visitDate: "09-12-20",
-  },
-  {
-    index: 3,
-    type: 0,
-    name: "Restaurant 3",
-    visitDate: "09-12-20",
-  },
-  {
-    index: 4,
-    type: 1,
-    name: "Restaurant 4",
-    visitDate: "09-12-20",
-  },
-  {
-    index: 5,
-    type: 0,
-    name: "Restaurant 5",
-    visitDate: "09-12-20",
-  },
-  {
-    index: 6,
-    type: 1,
-    name: "Restaurant 6",
-    visitDate: "09-12-20",
-  },
-  {
-    index: 7,
-    type: 0,
-    name: "Restaurant 7",
-    visitDate: "09-12-20",
-  },
-  {
-    index: 8,
-    type: 1,
-    name: "Restaurant 8",
-    visitDate: "09-12-20",
-  },
-  {
-    index: 9,
-    type: 0,
-    name: "Restaurant 9",
-    visitDate: "09-12-20",
-  },
-];*/
-
 const cards = [];
-
-
-
 
 /**
  * @param index The index of the card that has been swiped on
@@ -88,21 +26,22 @@ const RecentVisits: React.FC<Props> = (props) => {
 
   //Add visits to the card list
   var i = 0;
-  while (i < 1){
-  props.restaurantDetails.forEach(element => {
-    cards.push({
-      index: element.index, 
-      type: element.type, name: 
-      element.restaurantName, 
-      visitDate: element.restaurantVisitDate})
-    //Only display the 10 most recent visits
-    if (cards.length > 10) {
-      cards.pop();
-    }
-    i = i + 1;
-  });
-}
-
+  if (props.restaurantDetails.length > 0){
+   while (i < 1){
+     props.restaurantDetails.forEach(element => {
+        cards.push({
+         index: element.index, 
+         type: element.type, name: 
+         element.restaurantName, 
+         visitDate: element.restaurantVisitDate})
+       //Only display the 10 most recent visits
+       if (cards.length > 10) {
+          cards.pop();
+       }
+        i = i + 1;
+     });
+   }
+  }
   return <AnimatedScroll handleSwipe={handleSwipe} cards={cards} />;
 };
 
