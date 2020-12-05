8216da86033d1d82e1b846adbebc36347c387800
ReefLakin <reeflakin2@gmail.com>
2020-12-05 14:19:43 +0000

    Created Fun-CountDelimiters
    A new function has been created. 'CountDelimiters' quantifies the total number of dollar-symbol delimiters inside of a string using short clever code and returns them.

--- docs/database/ProcSetup.sql
+++ docs/database/ProcSetup.sql
@@ -4,6 +4,10 @@ SQL Procedure Setup:
     It will delete procedures if they currently exist in the database.
     The procedures are explained within this comment bracket of the script.
 
+    Within this file, I've also kept the stored functions we'll be using too.
+    They will also be deleted if they currently exist when this script is run into the database.
+
+
     Run-RemoveUser is a substitute for deleting a user record. Since we do not allow direct removal, this will do a few things:
         - Sets IsDeleted to true.
         - Eliminates all sensitive data.
@@ -15,13 +19,31 @@ SQL Procedure Setup:
         - Does not work on a user if they own a restaurant.
         - All other records will be eliminated via a simple ON DELETE CASCADE command.
         - For safety purposes, you must enter the user's database ID.
+
+    Func-RandomSelection is a function used to select one random inserted data item from a list of them.
+        - There is no array variable in MySQL; input a varchar string where each selectable value is proceeded by a $ symbol.
+        - The function then does a lot of clever things and eventually returns one item from the makeshift list.
+
+    Func-CountDelimiters is a function used to count how many times a $ symbol appears in a given string.
+        - A string is inserted into the function.
+        - An integer is outputted.
+        - Only $ symbols are counted at this current time.
+        - A fine piece of magic this one.
 */
+
+
+-- TO DO: Implement best-practice error handling techniques into these SPs.
+
 
 DROP PROCEDURE IF EXISTS `Run-RemoveUser`;
 DROP PROCEDURE IF EXISTS `Run-PermaDeleteUser`;
+DROP PROCEDURE IF EXISTS `Run-GenerateUserData`;
+
+DROP FUNCTION IF EXISTS `Func-RandomSelection`;
+DROP FUNCTION IF EXISTS `Func-CountDelimiters`;
 
 DELIMITER //
-CREATE PROCEDURE `Run-RemoveUser` (IN input_email VARCHAR(60)) -- TO DO: Implement best-practice error handling techniques into this SP & deal with restaurant owner problem.
+CREATE PROCEDURE `Run-RemoveUser` (IN input_email VARCHAR(60)) -- TO DO: Deal with restaurant owner problem.
 BEGIN
 	UPDATE `User`
     SET Email = '-1', `Password` = '-1', Nickname = '-1', IsDeleted = 1, IsVerified = 0
@@ -29,10 +51,36 @@ BEGIN
 END //
 
 
-CREATE PROCEDURE `Run-PermaDeleteUser` (IN input_id INT) -- TO DO: Implement best-practice error handling techniques into this SP.
+CREATE PROCEDURE `Run-PermaDeleteUser` (IN input_id INT)
 BEGIN
     DELETE FROM `User`
     WHERE UserID = input_id;
+END //
+
+
+CREATE PROCEDURE `Run-GenerateUserData` (IN input_quantity INT)
+BEGIN
+
+END //
+
+
+
+
+CREATE FUNCTION `Func-RandomSelection` (select_these VARCHAR(MAX))
+RETURNS VARCHAR(MAX)
+DETERMINISTIC
+BEGIN
+
+END //
+
+
+CREATE FUNCTION `Func-CountDelimiters` (input_string VARCHAR(MAX))
+RETURNS INT
+DETERMINISTIC
+BEGIN
+    DECLARE delCount INT; -- Counter is declared here.
+    SET delCount = (LENGTH(input_string) - LENGTH(REPLACE(input_string, "$", ""))); -- Counter is filled with a value equal to number of delimiters.
+    RETURN delCount; -- Counter value is returned.
 END //
 
 
