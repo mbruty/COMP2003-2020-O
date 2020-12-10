79f0b5e4a5516ee89fd0ffecf6f2b166b651cde2
JWKMachar <57765783+JWKMachar@users.noreply.github.com>
2020-12-10 12:47:00 +0000

    Updated Caching System
    -Redis Searching created
    -Redis Inserting created
    -Formatting to and from JSON
    
    NOTES:
    - place holder comment in the "primary key" yet to decide on the unique naming scheme

--- api/Backend/Data/Redis/CacheTable.cs
+++ api/Backend/Data/Redis/CacheTable.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
+
 
 namespace api.Backend.Data.SQL.Caching
 {
@@ -18,19 +21,38 @@ namespace api.Backend.Data.SQL.Caching
         public override async Task<T[]> Select<T>(object[] PrimaryKeyValues, int Limit = 0)
         {
             //Logic To Find And Return A Cached Object
-            return await base.Select<T>(PrimaryKeyValues,Limit);
+            string KeySearch = PrimaryKeyValues.ToString();
+            if (await Redis.Instance.HasKey(KeySearch))
+            {
+                return JToken.Parse(await Redis.Instance.GetString(KeySearch)).ToObject<T[]>();
+            }
+            T[] CachedObject = CacheObject(await base.Select<T>(PrimaryKeyValues, Limit));
+            return CachedObject;
         }
 
         public override async Task<T[]> SelectCustom<T>(string where = "TRUE", int Limit = 0)
         {
-            //Logic To Find And Return A Cached Object
             return await base.Select<T>(where,Limit);
         }
 
         public override async Task<T[]> Select<T>(string[] FieldNames, object[] FieldValues, int Limit = 0)
         {
             //Logic To Find And Return A Cached Object
-            return await base.Select<T>(FieldNames, FieldValues,Limit);
+            string FieldSearch = FieldNames.ToString();
+            if (await Redis.Instance.HasKey(FieldSearch))
+            {
+                return JToken.Parse(await Redis.Instance.GetString(FieldSearch)).ToObject<T[]>();
+            }
+            T[] CachedObject = CacheObject(await base.Select<T>(FieldNames, FieldValues, Limit));
+            return CachedObject;
+        }
+
+        /* Take Object and Convert to JSON format before adding to the Redis Database */
+        private T[] CacheObject<T>(T[] UncachedObject) where T : Object, new()
+        {
+            string CacheableObject = JsonConvert.SerializeObject(UncachedObject);
+            Redis.Instance.SetStringWithExpiration( /* PRIMARY KEY */ , CacheableObject);
+            return UncachedObject;
         }
     }
 }
