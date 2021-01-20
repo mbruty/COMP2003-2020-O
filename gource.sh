24663d087343420d6f42979313bf4cc52d4bb8d4
Oscar Davies <oscar.william.davies@outlook.com>
2021-01-20 10:46:17 +0000

    Modify Gource

--- gource.sh
+++ gource.sh
@@ -1,3 +1,2 @@
-cd 'G:\OneDrive - University of Plymouth\Y2S1\COMP2003\COMP2003-2020-O'
 gource --camera-mode track --seconds-per-day 1 --camera-mode overview -f -1920x1080 --bloom-multiplier 1.2 --bloom-intensity 1.2 --max-file-lag 0.5 #-o 'G:\Users\oscar\OneDrive\Documents\gource.ppm'
 read
