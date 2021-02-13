f3b3638aa2151d952f75b1acf55c2f22bf065b1c
Michael Bruty <michael.bruty@students.plymouth.ac.uk>
2021-02-13 14:03:28 +0000

    Fixing server part 2

--- website/server.js
+++ website/server.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, "./build")));
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+  res.sendFile(path.join(__dirname + "/build/index.html"));
 });
 
 const port = process.env.PORT || 5000;
