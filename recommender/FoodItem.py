985f94f7aea8dd2cbb8418c04c35c4100041525b
JWKMachar <57765783+JWKMachar@users.noreply.github.com>
2021-02-26 10:38:17 +0000

    FoodItem Comments
    - added descriptions of each function

--- recommender/FoodItem.py
+++ recommender/FoodItem.py
@@ -1,17 +1,22 @@
 class FoodItem:
+    #initialier for the class
     def __init__(self, inID, inFoodTags):
         self.ID = inID
         self.FoodTags = inFoodTags
-        
+    
+    #prints all relevant info stored on the class (for testing)
     def printInfo(self):
         print("ID: " + str(self.ID) + " - (" + str(type(self.ID)) + ")")
         print("FoodTags: " + str(self.FoodTags) + " - (" + str(type(self.FoodTags)) + ")")
 
+    #adds a new foodtag
     def addFoodTag(self, newFoodTag):
         self.FoodTags.add(newFoodTag)
+
+
 
 #CLASS TESTING CODE
 #Burger = FoodItem(1, { 1, 2, 3})
 #Burger.printInfo()
-#Burger.addFoodTag(4)
+#Burger.addFoodTag(3)
 #Burger.printInfo()
