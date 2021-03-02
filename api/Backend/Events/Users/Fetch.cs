97dad9c41ccd3a0b3f5c1ee0866378eae41d0789
Oscar Davies <oscar.william.davies@outlook.com>
2021-03-02 12:34:15 +0000

    Get Recent Visits (Recent 20)

--- api/Backend/Events/Users/Fetch.cs
+++ api/Backend/Events/Users/Fetch.cs
@@ -4,6 +4,7 @@ using api.Backend.Endpoints;
 using api.Backend.Security;
 using System.Collections.Specialized;
 using System.Threading.Tasks;
+using System.Linq;
 
 namespace api.Backend.Events.Users
 {
@@ -17,6 +18,16 @@ namespace api.Backend.Events.Users
             User[] users = await Binding.GetTable<User>().Select<User>("userid", headers["userid"]);
 
             response.AddObjectToData("user", users[0]);
+            response.StatusCode = 200;
+        }
+
+        [WebEvent("/fetch/user/visits", "GET", false, SecurityGroup.User)]
+        public static async Task GetRecentVisits(NameValueCollection headers, string Data, WebRequest.HttpResponse response)
+        {
+            User[] users = await Binding.GetTable<User>().Select<User>("userid", headers["userid"]);
+
+            Visit[] visits = await users[0].GetVisits();
+            response.AddObjectToData("visits", visits.OrderBy(x => x.DateOfVisit).Take(20).ToArray());
             response.StatusCode = 200;
         }
 
