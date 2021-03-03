6c583960c94fcde8755661736938819b164d5843
Michael Bruty <michael.bruty@students.plymouth.ac.uk>
2021-03-03 14:31:09 +0000

    Resizing images and fixed cors

--- image-handler/src/index.js
+++ image-handler/src/index.js
@@ -1,9 +1,10 @@
 const { ApolloServer, gql } = require("apollo-server-express");
-const { createWriteStream } = require("fs");
 const path = require("path");
 const express = require("express");
 const { Storage } = require("@google-cloud/storage");
-
+const sharp = require("sharp");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 const files = [];
 
 const typeDefs = gql`
@@ -17,25 +18,33 @@ const typeDefs = gql`
 
 const gc = new Storage({
   keyFilename: path.join(__dirname, "../track-and-taste-cee43c7463f3.json"),
-  projectId: "track-and-taste"
+  projectId: "track-and-taste",
 });
 
 const imgBucket = gc.bucket("tat-image");
 
+const transformer = sharp().resize({
+  width: 200,
+  height: 200,
+  fit: sharp.fit.contain,
+  position: sharp.strategy.centre,
+});
+
 const resolvers = {
   Query: {
-    files: () => files
+    files: () => files,
   },
   Mutation: {
     uploadFile: async (_, { file }) => {
       const { createReadStream, filename } = await file;
-
-      await new Promise(res =>
+      console.log(_);
+      await new Promise((res) =>
         createReadStream()
+          .pipe(transformer)
           .pipe(
             imgBucket.file(filename).createWriteStream({
               resumable: false,
-              gzip: true
+              gzip: true,
             })
           )
           .on("finish", res)
@@ -44,16 +53,32 @@ const resolvers = {
       files.push(filename);
 
       return true;
-    }
-  }
+    },
+  },
 };
 
-const server = new ApolloServer({ typeDefs, resolvers });
+const context = ({ req, res }) => {
+  // Set this header to the url when pushing to production
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+  // Do authentication here
+  console.log(req.cookies);
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context,
+});
 const app = express();
-app.get("*", (req, res, next) => {
-  console.log(req)
-  next();
-})
+app.use(cookieParser());
+app.use(
+  cors({
+    credentials: true,
+    origin: (origin, asd) => {
+      asd(null, true);
+    },
+  })
+);
 server.applyMiddleware({ app });
 
 app.listen(4000, () => {
--- image-handler/web-example/src/apollo.js
+++ image-handler/web-example/src/apollo.js
@@ -2,9 +2,12 @@ import ApolloClient from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { createUploadLink } from "apollo-upload-client";
 
-const link = createUploadLink({ uri: "http://localhost:4000/graphql" });
+const link = createUploadLink({
+  uri: "http://localhost:4000/graphql",
+  credentials: "include",
+});
 
 export const client = new ApolloClient({
   link,
-  cache: new InMemoryCache()
+  cache: new InMemoryCache(),
 });
