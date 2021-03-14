2a71431b1263780cfc2e131725688cf30f4f7c2d
Michael Bruty <michael.bruty@students.plymouth.ac.uk>
2021-03-14 13:13:36 +0000

    Updating to newer apollo

--- website/src/apollo.js
+++ website/src/apollo.js
@@ -1,10 +1,8 @@
-import ApolloClient from "apollo-client";
-import { InMemoryCache } from "apollo-cache-inmemory";
 import { createUploadLink } from "apollo-upload-client";
+import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const link = createUploadLink({
-  uri: "https://image-upload-4gqpkawhia-lz.a.run.app/graphql",
-  credentials: "include",
+  uri: "http://localhost:8080/graphql",
 });
 
 export const client = new ApolloClient({
