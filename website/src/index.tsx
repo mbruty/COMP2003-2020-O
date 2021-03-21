a63cf0737c448719b857ab343ad54b8547631dbb
Michael Bruty <michael.bruty@students.plymouth.ac.uk>
2021-03-21 13:25:52 +0000

    Cause authentication checks to happen for all pages automatically

--- website/src/index.tsx
+++ website/src/index.tsx
@@ -1,18 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 import { ApolloProvider } from "@apollo/client";
 import { client } from "./apollo";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
