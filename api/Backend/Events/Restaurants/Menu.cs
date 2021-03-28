0e2454aed94501e70c2ac164c047ff821ea5bf64
Oscar Davies <oscar.william.davies@outlook.com>
2021-03-28 15:56:57 +0000

    Get Administrator Menus

--- api/Backend/Events/Restaurants/Menu.cs
+++ api/Backend/Events/Restaurants/Menu.cs
@@ -32,6 +32,25 @@ namespace api.Backend.Events.Restaurants
 
             Data.Obj.Menu menu = menus[0];
 
+            response.AddToData("message", "Fetched menu");
+            response.AddObjectToData("menu", menu);
+            response.StatusCode = 200;
+        }
+
+        [WebEvent(typeof(MenuBody), "/menu/me", "GET", false, SecurityGroup.Administrator)]
+        public static async Task GetAllRestaurant(MenuBody body, WebRequest.HttpResponse response, Security.SecurityPerm perm)
+        {
+            Table table = Binding.GetTable<Data.Obj.Menu>();
+            Data.Obj.Menu[] menus = await table.SelectCustom<Data.Obj.Menu>(
+                what: "tat.Menu.MenuID,tat.Menu.MenuName,tat.Menu.IsChildMenu",
+                tables: "tat.Menu, tat.LinkMenuRestaurant, tat.Restaurant",
+                where: "(tat.Menu.MenuID=tat.LinkMenuRestaurant.MenuID AND tat.LinkMenuRestaurant.RestaurantID = tat.Restaurant.RestaurantID AND tat.Restaurant.OwnerID=@OID)",
+                new System.Collections.Generic.List<System.Tuple<string, object>>()
+                {
+                    new System.Tuple<string, object>("OID",perm.admin_id)
+                }
+                ); 
+
             //if (menu.OwnerID != perm.admin_id)
             //{
             //    response.StatusCode = 401;
@@ -40,7 +59,7 @@ namespace api.Backend.Events.Restaurants
             //}
 
             response.AddToData("message", "Fetched menu");
-            response.AddObjectToData("menu", menu);
+            response.AddObjectToData("menus", menus);
             response.StatusCode = 200;
         }
     }
