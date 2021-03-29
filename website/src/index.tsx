84dde5e9522fa5ed7cc084a860ce076387d82046
Michael Bruty <michael.bruty@students.plymouth.ac.uk>
2021-03-29 11:27:48 +0100

    Wrapping in picker utils

--- website/src/index.tsx
+++ website/src/index.tsx
@@ -6,12 +6,16 @@ import * as serviceWorker from "./serviceWorker";
 import { ApolloProvider } from "@apollo/client";
 import { client } from "./apollo";
 import { BrowserRouter } from "react-router-dom";
+import { MuiPickersUtilsProvider } from "@material-ui/pickers";
+import MomentUtils from "@date-io/moment";
 
 ReactDOM.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
       <BrowserRouter>
-        <App />
+        <MuiPickersUtilsProvider utils={MomentUtils}>
+          <App />
+        </MuiPickersUtilsProvider>
       </BrowserRouter>
     </ApolloProvider>
   </React.StrictMode>,
