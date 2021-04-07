6a7a6d4d650e5f93f2a203041903004e2f1f4b5a
Michael Bruty <michael.bruty@students.plymouth.ac.uk>
2021-04-07 21:40:39 +0100

    Added endpoint to get a food item by id

--- api/Backend/Events/FoodItems/Fetch.cs
+++ api/Backend/Events/FoodItems/Fetch.cs
@@ -25,9 +25,52 @@ namespace api.Backend.Events.FoodItems
                 }
                 );
 
-            response.AddToData("message", "Fetched menu");
             response.AddObjectToData("items", items);
             response.StatusCode = 200;
         }
+
+
+
+
+        // Get all the info for the item builder
+        [WebEvent(typeof(string), "/fooditem/verbose/:id:", "GET", false, SecurityGroup.Administrator)]
+        public static async Task GetFoodItemByID(string id, WebRequest.HttpResponse response, Security.SecurityPerm perm)
+        {
+            // Get the food items
+            Table table = Binding.GetTable<Data.Obj.FoodItem>();
+            Data.Obj.FoodItem[] items = await table.SelectCustom<Data.Obj.FoodItem>(
+                what: "DISTINCT FoodItem.*",
+                tables: "tat.FoodItem, tat.LinkMenuFood, tat.Menu, tat.LinkMenuRestaurant, tat.Restaurant, tat.RestaurantAdmin",
+                where: "(tat.FoodItem.FoodID=tat.LinkMenuFood.FoodID AND tat.LinkMenuFood.FoodID = tat.Menu.MenuID AND tat.Menu.MenuID = tat.LinkMenuRestaurant.MenuID AND tat.LinkMenuRestaurant.RestaurantID = tat.Restaurant.RestaurantID AND tat.Restaurant.OwnerID=@OID AND tat.FoodItem.FoodID=@FID)",
+                new System.Collections.Generic.List<System.Tuple<string, object>>()
+                {
+                    new System.Tuple<string, object>("OID",perm.admin_id),
+                    new System.Tuple<string, object>("FID", id)
+                }
+                );
+
+            // We should only be getting 1 item so can just assume it's at [0]
+            // Get the food checks
+            Data.Obj.FoodChecks[] checks = await Binding.GetTable<Data.Obj.FoodChecks>().Select<Data.Obj.FoodChecks>("FoodCheckID", items[0].FoodCheckID);
+
+            // Get the tags
+            Table t2 = Binding.GetTable<Data.Obj.FoodTags>();
+            Data.Obj.FoodTags[] tags = await t2.SelectCustom<Data.Obj.FoodTags>(
+                what: "tat.FoodTags.*",
+                tables: "tat.FoodTags, tat.FoodItemTags",
+                where: "tat.FoodTags.FoodTagID = tat.FoodItemTags.TagID AND tat.FoodItemTags.FoodID = @FID",
+                new System.Collections.Generic.List<System.Tuple<string, object>>()
+                {
+                    new System.Tuple<string, object>("FID", id)
+                }
+                );
+
+
+            response.AddObjectToData("fooditem", items);
+            response.AddObjectToData("checks", checks[0]);
+            response.AddObjectToData("tags", tags);
+            response.StatusCode = 200;
+        }
+
     }
 }
