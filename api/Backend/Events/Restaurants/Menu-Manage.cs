b1f7c13aded4e849c0756789f76e2e4d91176060
Oscar Davies <oscar.william.davies@outlook.com>
2021-04-11 10:58:26 +0100

    Link Restaurant & Add Time

--- api/Backend/Events/Restaurants/Menu-Manage.cs
+++ api/Backend/Events/Restaurants/Menu-Manage.cs
@@ -5,9 +5,23 @@ using api.Backend.Security;
 using Newtonsoft.Json;
 using System.Collections.Specialized;
 using System.Threading.Tasks;
+using System;
 
 namespace api.Backend.Events.Restaurants
 {
+    public class MenuTimeBody
+    {
+        public uint MenuRestID;
+        public string DayRef;
+        public TimeSpan StartServing, TimeServing;
+    }
+
+    public class LinkMenuRestaurantBody
+    {
+        public uint MenuRestID, MenuID, RestaurantID;
+        public bool AlwaysServe, IsActive;
+    }
+
     public static class Menu_Manage
     {
         [WebEvent(typeof(MenuBody), "/menu/create", "POST", false, SecurityGroup.Administrator)]
@@ -24,6 +38,41 @@ namespace api.Backend.Events.Restaurants
 
             response.AddToData("message", "Created menu");
             response.AddObjectToData("menu", _menu);
+            response.StatusCode = 200;
+        }
+
+        [WebEvent(typeof(LinkMenuRestaurantBody), "/menu/linkrestaurant", "POST", false, SecurityGroup.Administrator)]
+        public static async Task LinkMenuToRestaurant(LinkMenuRestaurantBody body, WebRequest.HttpResponse response, Security.SecurityPerm perm)
+        {
+            Data.Obj.LinkMenuRestaurant _link = new Data.Obj.LinkMenuRestaurant() { MenuID = body.MenuID, RestaurantID = body.RestaurantID, IsActive = body.IsActive, AlwaysServe = body.AlwaysServe };
+
+            if (!await _link.Insert(true))
+            {
+                response.StatusCode = 401;
+                response.AddToData("error", "Something went wrong!");
+                return;
+            }
+
+            response.AddToData("message", "Created link");
+            response.AddObjectToData("link", _link);
+            response.StatusCode = 200;
+        }
+
+        [WebEvent(typeof(MenuTimeBody), "/menu/addtime", "POST", false, SecurityGroup.Administrator)]
+        public static async Task AddTimeToRestaurant(MenuTimeBody body, WebRequest.HttpResponse response, Security.SecurityPerm perm)
+        {
+            Data.Obj.MenuTimes _time = new Data.Obj.MenuTimes() { DayRef = body.DayRef, MenuRestID = body.MenuRestID, StartServing = body.StartServing, TimeServing = body.TimeServing };
+
+#warning needs updating
+            if (!await _time.Insert(false)) 
+            {
+                response.StatusCode = 401;
+                response.AddToData("error", "Something went wrong!");
+                return;
+            }
+
+            response.AddToData("message", "Created time");
+            response.AddObjectToData("time", _time);
             response.StatusCode = 200;
         }
     }
