7e0bc9b6a6513f5ed61631482aaf78b44252721f
AlexDenman47674 <alexander.denman@students.plymouth.ac.uk>
2021-04-16 11:12:41 +0100

    Created food tags page tsx file

---
+++ website/src/Community-food-tags/CommunityFoodTags.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+interface Props {}
+
+const TagBuilder: React.FC<Props> = () => {
+    return (
+        <div className="content">
+            <h1>Welcome to the Track and Taste Community Food Tags Page</h1>
+            <h3>Let's create a new item for your menu</h3>
+            <span><hr/></span>
+
+            </div>
+    );
+};
+
+export default TagBuilder;
