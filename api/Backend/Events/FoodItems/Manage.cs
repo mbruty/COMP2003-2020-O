2fbbcab518f4c52aaba9b3c43758c01b78bbfd85
Michael Bruty <michael.bruty@students.plymouth.ac.uk>
2021-04-24 13:49:08 +0100

    Updated to new version of db

--- api/Backend/Events/FoodItems/Manage.cs
+++ api/Backend/Events/FoodItems/Manage.cs
@@ -3,18 +3,28 @@ using api.Backend.Data.SQL.AutoSQL;
 using api.Backend.Endpoints;
 using api.Backend.Security;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace api.Backend.Events.FoodItems
 {
     public class FoodItemBody
     {
-        public uint FoodID, MenuID;
+        public uint FoodID, MenuID, Creator;
 
         public string FoodName, FoodNameShort, FoodDescription;
 
         public decimal Price;
 
+        public FoodTagsBody[] Tags;
+
+        public FoodChecks Checks;
+
+    }
+
+    public class FoodTagsBody
+    {
+        public uint FoodTagID;
     }
 
     public static class Manage
@@ -23,7 +33,17 @@ namespace api.Backend.Events.FoodItems
         [WebEvent(typeof(FoodItemBody), "/fooditem/create", "POST", false, SecurityGroup.Administrator)]
         public static async Task CreateRestaurant(FoodItemBody body, WebRequest.HttpResponse response, Security.SecurityPerm perm)
         {
-            Data.Obj.FoodItem _item = new Data.Obj.FoodItem() { FoodDescription = body.FoodDescription, FoodName = body.FoodName, FoodNameShort = body.FoodNameShort, Price = body.Price };
+            Data.Obj.FoodChecks _checks = body.Checks;
+
+            if (! await _checks.Insert(true))
+            {
+                response.StatusCode = 401;
+                response.AddToData("error", "Something went wrong!");
+                return;
+            }
+
+            Data.Obj.FoodItem _item = new Data.Obj.FoodItem() { FoodDescription = body.FoodDescription, FoodName = body.FoodName, FoodNameShort = body.FoodNameShort, Price = body.Price, Creator = perm.admin_id, FoodCheckID = _checks.FoodCheckID };
+
 
             if (!await _item.Insert(true))
             {
@@ -32,15 +52,16 @@ namespace api.Backend.Events.FoodItems
                 return;
             }
 
-            LinkMenuFood _link = new LinkMenuFood() { FoodID = _item.FoodID, MenuID = body.MenuID };
+            List<Task> tasks = new List<Task>();
 
-            if (!await _link.Insert(false))
+            foreach(FoodTagsBody ft in body.Tags)
             {
-                response.StatusCode = 401;
-                response.AddToData("error", "Something went wrong!");
-                await _item.Delete();
-                return;
+                tasks.Add(
+                    new FoodItemTags() { FoodID = _item.FoodID, TagID = ft.FoodTagID }.Insert()
+                );
             }
+
+            await Task.WhenAll(tasks);
 
             response.AddToData("message", "Created food item");
             response.AddObjectToData("item", _item);
