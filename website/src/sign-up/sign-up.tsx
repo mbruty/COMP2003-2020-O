48ee03212a8900f211a3a1808a9bb51e1c3cebe9
AlexDenman47674 <alexander.denman@students.plymouth.ac.uk>
2021-04-29 12:09:44 +0100

    created blank sign up tsx file

---
+++ website/src/sign-up/sign-up.tsx
@@ -0,0 +1,23 @@
+import React, { useEffect } from "react";
+import Checkbox from "@material-ui/core/Checkbox";
+import FormGroup from "@material-ui/core/FormGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import TextField from "@material-ui/core/TextField";
+import Autocomplete from "@material-ui/lab/Autocomplete";
+import {
+  Button,
+  Collapse,
+  Divider,
+  IconButton,
+  Paper,
+} from "@material-ui/core";
+
+interface Props {}
+
+const signUp: React.FC<Props> = () => {
+  return(
+      <div className="content">
+          
+      </div>
+  )
+}
