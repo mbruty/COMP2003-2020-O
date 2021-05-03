469fda956ea9db4d1dfecca64fbe410b80461a7c
mbruty <michael.bruty@students.plymouth.ac.uk>
2021-05-03 18:05:50 +0100

    Registering for push notifications and wrapping app in auth context

--- mobile-app/App.tsx
+++ mobile-app/App.tsx
@@ -1,31 +1,45 @@
 import React, { useEffect, useState } from "react";
-import { SafeAreaView, StyleSheet, Text, View } from "react-native";
-import { includeAuth, reset } from "./src/packages/includeAuth";
+import { Platform, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { auth, includeAuth, reset } from "./src/packages/includeAuth";
 import MainScreen from "./src/packages/main-sequence";
 import { LogIn, SignUpProcess } from "./src/packages/user";
 import authCheck from "./src/packages/requests/authCheck";
 import { IUser } from "./src/packages/user/IUser";
+import * as Notifications from "expo-notifications";
 import { Loading } from "./src/Loading";
+import Constants from "expo-constants";
+import * as Permissions from "expo-permissions";
+import { AuthContext } from "./src/AuthContext";
 
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: true,
+    shouldSetBadge: true,
+  }),
+});
 
 export default function App() {
-  const [user, setUser] = useState<IUser>();
+  const [user, setUser] = useState<auth>(undefined);
   const [isLoggedIn, setIsLoggedIn] = useState<boolean>(false);
   /* For development set this to the page you're making..
   Set this to "main" when publishing */
 
   const [page, setPage] = useState<string>("loading");
-
+  useEffect(() => {
+    registerForPushNotifications().then(console.log).catch(console.log);
+  }, []);
   const logIn = () => {
     setIsLoggedIn(true);
+    includeAuth().then((authObj) => setUser(authObj));
     setPage("main");
   };
 
   const logOut = () => {
-    console.log("logging out")
+    console.log("logging out");
     reset();
     setIsLoggedIn(false);
-  }
+  };
 
   useEffect(() => {
     authCheck().then((loggedIn) => {
@@ -34,6 +48,9 @@ export default function App() {
         setPage("log-in");
       } else {
         setPage("main");
+        includeAuth().then((authObj) => {
+          setUser(authObj);
+        });
       }
     });
   }, [isLoggedIn]);
@@ -41,32 +58,39 @@ export default function App() {
   includeAuth().then((res) => console.log(res));
 
   // Render the different pages by name of page
-  switch (page) {
-    case "sign-up":
-      return <SignUpProcess setUser={setUser} setPage={setPage} user={user} />;
-    case "log-in":
-      return (
-        <View style={styles.container}>
-          <LogIn setPage={setPage} submit={logIn} />
-        </View>
-      );
-    case "main":
-      return (
-        <View style={styles.container}>
-          <MainScreen logOut={logOut} />
-        </View>
-      );
-    case "loading":
-      return(
-        <Loading />
-      )
-    default:
-      return (
-        <View style={styles.container}>
-          <Text>There was an error loading the app...</Text>
-        </View>
-      );
-  }
+  const renderPage = () => {
+    switch (page) {
+      case "sign-up":
+        return (
+          <SignUpProcess setUser={setUser} setPage={setPage} user={user} />
+        );
+      case "log-in":
+        return (
+          <View style={styles.container}>
+            <LogIn setPage={setPage} submit={logIn} />
+          </View>
+        );
+      case "main":
+        return (
+          <View style={styles.container}>
+            <MainScreen logOut={logOut} />
+          </View>
+        );
+      case "loading":
+        return <Loading />;
+      default:
+        return (
+          <View style={styles.container}>
+            <Text>There was an error loading the app...</Text>
+          </View>
+        );
+    }
+  };
+  console.log({ user });
+
+  return (
+    <AuthContext.Provider value={user}>{renderPage()}</AuthContext.Provider>
+  );
 }
 
 const styles = StyleSheet.create({
@@ -77,3 +101,28 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
 });
+
+const registerForPushNotifications = async function () {
+  if (Constants.isDevice) {
+    // Get the notifications permission
+    const { status: existingStatus } = await Permissions.getAsync(
+      Permissions.NOTIFICATIONS
+    );
+
+    let finalStatus = existingStatus;
+
+    if (existingStatus !== "granted") {
+      const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+      finalStatus = status;
+    }
+
+    if (finalStatus !== "granted") {
+      return;
+    }
+
+    // If the permission was granted, then get the token
+    const token = await Notifications.getExpoPushTokenAsync();
+
+    return token;
+  }
+};
